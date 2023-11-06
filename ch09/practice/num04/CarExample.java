709e5718eb703beefb18c19639ef33231f38a11c
ina <jia3527@gmail.com>
2023-11-06 22:37:43 +0200

    ch09 prac revision

--- ch09/practice/num04/CarExample.java
+++ ch09/practice/num04/CarExample.java
@@ -1,12 +1,10 @@
 package ch09.practice.num04;
 
-import myCar.Tire;
-
 public class CarExample {
     public static void main (String[] args) {
         Car myCar = new Car();
 
-        Tire tire = new myCar.Tire();
+        Car.Tire tire = myCar.new Tire();
         Car.Engine engine = new Car.Engine();
     }
 }
--- myCar/Tire.java
+++
@@ -1,5 +0,0 @@
-package myCar;
-
-public class Tire {
-
-}
